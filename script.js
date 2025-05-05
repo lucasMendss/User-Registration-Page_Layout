f7724cb8584ec604327f06001969e848826b87c1
lucas <87384661+lucasMendss@users.noreply.github.com>
2025-05-05 15:42:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function trocarImagem() {
     const imgStatusSenha = document.getElementById("status_senha");
     const senhaInputType = document.getElementById("caixa_senha");
 
-   if (imgStatusSenha.src.includes("/assets/visible.png")) {
+   if (imgStatusSenha.src.includes("./assets/visible.png")) {
         imgStatusSenha.src = "./assets/hidden.png";
         senhaInputType.type = "text";
    }
